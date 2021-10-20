c6600f52693ffdbf7db82a6e622e5c467f0d5018
venomz75 <vc75@me.com>
2021-10-20 04:50:56 +0100

    Implemented heuristic to greatly improve efficiency
    Can now solve 8x8 and larger knights tours in much fewer attempted moves.

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # knightsTour
- Recursively tries to solve the Knight's Tour problem on a board of N length. Currently, a value of 5 works fine but any increase in N increases time complexity exponentially. 
- 
- I'm looking at optimising the algorithm so I may solve the tour on an 8*8 grid but for now this is a simple yet inefficient implementation.
+ Recursively tries to solve the Knight's Tour problem on a board of N length (8 by default) using Warnsdorff's Rule.
--- knightsTour.cpp
+++ knightsTour.cpp
@@ -3,17 +3,24 @@
 #include <iostream>
 #include <iomanip>
 #include <vector>
+#include <list>
+#include <thread>
+#include <algorithm>
 using namespace std;
 
 //global variables
-int tries = 0; int N = 5;
+int tries = 0; int N = 8;
+
 vector<vector<int>> board;
+vector<vector<int>> moveVectors 
+{
+    {2,2,1,1,-1,-1,-2,-2},
+    {1,-1,2,-2,2,-2,1,-1}
+};
 
 //enums for state of spaces on board
 enum boardState {
-    not_visited = -1,
-    visited = 1,
-    knight = 2
+    not_visited = -1
 };
 
 //initialise grid of size N*N
@@ -24,14 +31,15 @@ void init() {
 //draw grid to console
 void draw() {
     for (int y = 0; y < N; y++) {
+        cout << "| ";
         for (int x = 0; x < N; x++) {
             if (board[y][x] == not_visited) {
                 cout << "-- ";
             } else {
-                cout << setfill('0') << setw(2) << board[y][x] << " ";
+                cout << setfill('0') << setw(2) << board[y][x] << " | ";
             }
         }
-        cout << "\n";
+        cout << "\n" << string(5*N+1, '-') << "\n";
     }
 }
 
@@ -39,25 +47,46 @@ bool isValid(int y, int x) {
     if (y >= 0 && x >= 0 && y < N && x < N && board[y][x] == not_visited) return true;
 }
 
+int checkNextMoves(int y, int x) {
+    int availableMoves = 0;
+    for (int i = 0; i < 8; i++) {
+        if (isValid(y+moveVectors[0][i],x+moveVectors[1][i])) availableMoves++;
+    }
+    return availableMoves;
+}
+
 bool knightsTourRecursion(int y, int x, int n) {
     if (n == (N*N)+1) return true;
 
     if (isValid(y, x)) {
-        tries++;
-        draw();
-        cout << "n: " << n << "\n";
-        cout << "tries: " << tries << "\n";
-        cout << "\n";
-        
         board[y][x] = n;
-        if (knightsTourRecursion(y-2, x-1, n+1)) return true; //up 2 left 1
-        if (knightsTourRecursion(y-2, x+1, n+1)) return true; //up 2 right 1
-        if (knightsTourRecursion(y-1, x+2, n+1)) return true; //right 2 up 1
-        if (knightsTourRecursion(y+1, x+2, n+1)) return true; //right 2 down 1
-        if (knightsTourRecursion(y+2, x+1, n+1)) return true; //down 2 right 1
-        if (knightsTourRecursion(y+2, x-1, n+1)) return true; //down 2 left 1
-        if (knightsTourRecursion(y+1, x-2, n+1)) return true; //left 2 down 1
-        if (knightsTourRecursion(y-1, x-2, n+1)) return true; //left 2 up 1
+        tries++;
+
+        int nextMoves [8];
+        
+        for (int i = 0; i < 8; i++) {   
+            nextMoves[i] = checkNextMoves(y+moveVectors[0][i],x+moveVectors[1][i]);
+        }
+
+        list<int> indices;
+        for (int i = 0; i < 8; i++) {
+            int fewestMoves = 99;
+            int fewestMovesIndex = -1;
+            for (int j = 0; j < 8; j++) {
+                bool exists = find(indices.begin(), indices.end(), j) != indices.end();
+                if (nextMoves[j] < fewestMoves && !exists) {
+                    fewestMoves = nextMoves[j];
+                    fewestMovesIndex = j;
+                }
+            }
+            indices.push_back(fewestMovesIndex);
+        }
+
+        while (indices.size() > 0) {
+            if (knightsTourRecursion(y+moveVectors[0][indices.front()],x+moveVectors[1][indices.front()], n+1)) return true;
+            tries++;
+            indices.pop_front();
+        }
 
         board[y][x] = not_visited;
         return false;
@@ -76,7 +105,7 @@ int main() {
             knightsTourRecursion(i, j, 1);
         }
     }
-    
     draw();
+    cout << "Successfully completed Knights Tour of " << N << " by " << N << " grid with " << tries << " checks.\n";
     return 0;
 }
